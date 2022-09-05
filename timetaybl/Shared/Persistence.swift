b867bda3ab70f401ee0e0193592d29ad63eaf090
mohulshukla <mohul.shukla@gmail.com>
2022-09-05 19:23:31 +0200

    another test

--- timetaybl/Shared/Persistence.swift
+++ timetaybl/Shared/Persistence.swift
@@ -20,6 +20,8 @@ struct PersistenceController {
         do {
             try viewContext.save()
         } catch {
+            // test
+            // another test 
             // Replace this implementation with code to handle the error appropriately.
             // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
             let nsError = error as NSError
